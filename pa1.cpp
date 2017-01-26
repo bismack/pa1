9b15b5209e82843c9aa9703ee9e3d1cc2d926ad2
mathee <sivanm2@mcmaster.ca>
2017-01-26 17:13:38 -0500

    property validation + textfile output

--- pa1.cpp
+++ pa1.cpp
@@ -1,32 +1,77 @@
 #include <iostream>
 #include <cstdlib>
 #include <pthread.h>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-#define NUM_THREADS     5
+int numOfSegmentsSatisfied = 0;
 
-void *PrintHello(void *threadid) {
-   long tid;
-   tid = (long)threadid;
-   cout << "Hello World! Thread ID, " << tid << endl;
-   pthread_exit(NULL);
+bool verifyF(int propertyType, char c0, char c1, char c2, string segment) {
+
+   bool isSatisfied = false;
+   cout << "Validating Property F" << propertyType << ": " << endl;
+
+   size_t occurOfC0 = count(segment.begin(), segment.end(), c0);
+   size_t occurOfC1 = count(segment.begin(), segment.end(), c1);
+   size_t occurOfC2 = count(segment.begin(), segment.end(), c2);
+
+   cout << "Occurences of c0: " << occurOfC0 << endl;
+   cout << "Occurences of c1: " << occurOfC1 << endl;
+   cout << "Occurences of c2: " << occurOfC2 << endl;
+
+   switch (propertyType) {
+      case 0:
+         if (occurOfC0 + occurOfC1 == occurOfC2) {
+            isSatisfied = true;
+            cout << "YES" << endl;
+         } 
+         cout << "Property 0 Hit" << endl;
+         break;
+      case 1:
+         if (occurOfC0 + (2 * occurOfC1) == occurOfC2) {
+            isSatisfied = true;
+            cout << "YES" << endl;
+         } 
+         cout << "Property 1 Hit" << endl;
+         break;
+      case 2:
+         if (occurOfC0 * occurOfC1 == occurOfC2) {
+            isSatisfied = true;
+            cout << "YES" << endl;
+         } 
+         cout << "Property 2 Hit" << endl;
+         break;
+      case 3:
+         if (occurOfC0 - occurOfC1 == occurOfC2) {
+            isSatisfied = true;
+            cout << "YES" << endl;
+         } 
+         cout << "Property 3 Hit" << endl;
+         break;
+      default:
+         cout << "Property DEFAULT Hit" << endl;
+   }  
+
+   return isSatisfied;
 }
 
+
 int main () {
-   pthread_t threads[NUM_THREADS];
-   int rc;
-   int i;
-	
-   for( i=0; i < NUM_THREADS; i++ ){
-      cout << "main() : creating thread, " << i << endl;
-      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
-		
-      if (rc){
-         cout << "Error:unable to create thread," << rc << endl;
-         exit(-1);
-      }
+
+   ofstream textFile;
+   textFile.open("out.txt");
+
+
+   if (verifyF(1, 'a', 'b', 'c', "bccabcaccc") == true) {
+      numOfSegmentsSatisfied++;
+      cout << "numOfSegmentsSatisfied: " << numOfSegmentsSatisfied << endl;
    }
-	
-   pthread_exit(NULL);
+   
+   textFile << numOfSegmentsSatisfied;
+   textFile.close();
+
 }
